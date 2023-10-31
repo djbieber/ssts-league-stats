0b944b6ada09288a5cbb9a46b55e7e10eec92d00
djbieber <danbieber1991@gmail.com>
2023-10-30 20:24:08 -0400

    add new command

--- helpers/register_command.py
+++ helpers/register_command.py
@@ -5,10 +5,16 @@ import os
 url = f"https://discord.com/api/v10/applications/{os.getenv('APP_ID')}/commands"
 
 # This is an example CHAT_INPUT or Slash Command, with a type of 1
-json = {
+zen_json = {
     "name": "zen",
     "type": 1,
     "description": "Make zen"
+}
+
+log_a_match_json = {
+    "name":         "log_a_match",
+    "type":         1,
+    "description":  "After you play a tag match, use this command to log the results."
 }
 
 headers = {
@@ -16,6 +22,6 @@ headers = {
     "Content-Type": "application/json"
 }
 
-r = requests.post(url, headers=headers, json=json)
+r = requests.post(url, headers=headers, json=log_a_match_json)
 print(r.status_code)
 print(r.text)
